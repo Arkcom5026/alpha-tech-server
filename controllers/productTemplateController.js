1ec6f9d816234a80b73fc283d177cb922abd0c30
Arkcom5026 <advicebanphot@gmail.com>
2026-07-04 02:47:00 +0700

    chore(mission-c): remove legacy product template controller

--- controllers/productTemplateController.js
+++
@@ -1,5 +0,0 @@
-// controllers/productTemplateController.js
-// Mission C compatibility controller.
-// The legacy ProductTemplate model is retired. Product Template governance now uses the module implementation.
-
-module.exports = require('../src/modules/productTemplate/controllers/productTemplateController');
