69521a3a2c08730fb0b59a26b5fe99a4afdcc382
Arkcom5026 <advicebanphot@gmail.com>
2026-07-04 23:19:38 +0700

    refactor(quickstock): add singleton service wrapper

---
+++ src/modules/quickStock/services/QuickStockServiceSingleton.js
@@ -0,0 +1,10 @@
+const { prisma: sharedPrisma } = require('../../../../lib/prisma')
+const QuickStockService = require('./QuickStockService')
+
+class QuickStockServiceSingleton extends QuickStockService {
+  constructor(prisma = sharedPrisma) {
+    super(prisma)
+  }
+}
+
+module.exports = QuickStockServiceSingleton
