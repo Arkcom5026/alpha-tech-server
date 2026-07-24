d64f85cefc5f998cbbde5f5b210e88a7de038dc4
Arkcom5026 <advicebanphot@gmail.com>
2026-07-25 02:11:57 +0700

    refactor(employee): move employee persistence into repository boundary

--- src/modules/employee/repositories/employeeRepository.js
+++ src/modules/employee/repositories/employeeRepository.js
@@ -1,15 +1,43 @@
 const { prisma } = require('../../../lib/prisma');
 
+const employeeInclude = {
+  user: true,
+  position: true,
+  branch: true,
+};
+
+const findEmployees = ({ branchId } = {}) => prisma.employeeProfile.findMany({
+  where: branchId ? { branchId } : undefined,
+  include: employeeInclude,
+  orderBy: { id: 'desc' },
+});
+
 const findEmployeeById = (id) => prisma.employeeProfile.findUnique({
   where: { id },
-  include: { user: true, position: true, branch: true },
+  include: employeeInclude,
+});
+
+const findEmployeeUserByEmail = (email) => prisma.user.findUnique({
+  where: { email },
+  select: { id: true },
+});
+
+const findPositionById = (id) => prisma.position.findUnique({
+  where: { id },
+  select: { id: true, name: true },
 });
 
 const findPositions = () => prisma.position.findMany({
   orderBy: { name: 'asc' },
 });
 
+const createEmployeeTransaction = (callback) => prisma.$transaction(callback);
+
 module.exports = {
+  findEmployees,
   findEmployeeById,
+  findEmployeeUserByEmail,
+  findPositionById,
   findPositions,
+  createEmployeeTransaction,
 };
