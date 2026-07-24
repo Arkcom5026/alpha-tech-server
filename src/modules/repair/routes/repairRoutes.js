789ece1be080c813dd375b1b675272752839684c
chaiwat <advicebanphot@gmail.com>
2026-07-24 12:54:07 +0700

    chore: checkpoint before repair authorization review

--- src/modules/repair/routes/repairRoutes.js
+++ src/modules/repair/routes/repairRoutes.js
@@ -1,40 +1,76 @@
 const express = require('express');
 const verifyToken = require('../../../../middlewares/verifyToken');
 const repairController = require('../controllers/repairController');
+const {
+  loadRepairEmployeeContext,
+  allowRepairRoles,
+} = require('../middlewares/repairAuthorization');
 
 const router = express.Router();
 
+const READ_AND_INTAKE_ROLES = ['OWNER', 'MANAGER', 'CASHIER'];
+const OPERATION_ROLES = ['OWNER', 'MANAGER'];
+
 router.use(verifyToken);
+router.use(loadRepairEmployeeContext);
 
-router.get('/intake-context/:lookup', repairController.getIntakeContext);
+router.get(
+  '/intake-context/:lookup',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
+  repairController.getIntakeContext
+);
 
-router.get('/jobs', repairController.listJobs);
+router.get(
+  '/jobs',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
+  repairController.listJobs
+);
 
-router.get('/jobs/:id', repairController.getJob);
+router.get(
+  '/jobs/:id',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
+  repairController.getJob
+);
 
-router.post('/jobs', repairController.createJob);
+router.post(
+  '/jobs',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
+  repairController.createJob
+);
 
-router.patch('/jobs/:id/status', repairController.updateStatus);
+router.patch(
+  '/jobs/:id/status',
+  allowRepairRoles(...OPERATION_ROLES),
+  repairController.updateStatus
+);
 
-router.post('/jobs/:id/parts', repairController.addParts);
+router.post(
+  '/jobs/:id/parts',
+  allowRepairRoles(...OPERATION_ROLES),
+  repairController.addParts
+);
 
 router.post(
   '/jobs/:id/warranty-claims',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
   repairController.openWarrantyClaim
 );
 
 router.get(
   '/warranty-claims',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
   repairController.listWarrantyClaims
 );
 
 router.get(
   '/warranty-claims/:claimId',
+  allowRepairRoles(...READ_AND_INTAKE_ROLES),
   repairController.getWarrantyClaim
 );
 
 router.patch(
   '/warranty-claims/:claimId/status',
+  allowRepairRoles(...OPERATION_ROLES),
   repairController.updateWarrantyClaimStatus
 );
 
