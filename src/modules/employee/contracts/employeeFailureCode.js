de06786a4f2249c32d9f5e0cc8c7e2204182e423
Arkcom5026 <advicebanphot@gmail.com>
2026-07-25 02:02:25 +0700

    feat(employee): add employee module failure boundary

---
+++ src/modules/employee/contracts/employeeFailureCode.js
@@ -0,0 +1,7 @@
+const EmployeeFailureCode = Object.freeze({
+  NOT_FOUND: 'EMPLOYEE_NOT_FOUND',
+  FORBIDDEN: 'EMPLOYEE_FORBIDDEN',
+  INVALID_INPUT: 'EMPLOYEE_INVALID_INPUT',
+});
+
+module.exports = EmployeeFailureCode;
