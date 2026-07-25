3e618fa8ce9811e6b7ca6661c6dfe68db056b66a
Arkcom5026 <advicebanphot@gmail.com>
2026-07-25 10:12:53 +0700

    refactor(product): remove obsolete product route compatibility mount

--- routes/productRoutes.js
+++
@@ -1,4 +0,0 @@
-// Compatibility mount retained to preserve the existing application route wiring.
-// Runtime authority now belongs to the Product module router.
-
-module.exports = require('../src/modules/product/routes/productModuleRoutes')
