57201563b405e36919da5b6bad8717a6f74d5165
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 05:59:17 +0700

    refactor(procurement): route receipt operational endpoints to slices

--- routes/purchaseOrderReceiptRoutes.js
+++ routes/purchaseOrderReceiptRoutes.js
@@ -3,12 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-  finalizeReceiptController,
-  getReceiptsReadyToPay,
-  commitReceipt,
-} = require('../controllers/purchaseOrderReceiptController');
-
 const createPurchaseReceiptController = require('../src/modules/procurement/receipt/create/createPurchaseReceiptController');
 const createQuickReceiptController = require('../src/modules/procurement/receipt/quick/createQuickReceiptController');
 const updatePurchaseReceiptNoteController = require('../src/modules/procurement/receipt/update-note/updatePurchaseReceiptNoteController');
@@ -16,18 +10,21 @@ const deletePurchaseReceiptController = require('../src/modules/procurement/rece
 const listPurchaseReceiptsController = require('../src/modules/procurement/receipt/query/list/listPurchaseReceiptsController');
 const getPurchaseReceiptController = require('../src/modules/procurement/receipt/query/detail/getPurchaseReceiptController');
 const listReceiptItemsController = require('../src/modules/procurement/receipt/query/items/listReceiptItemsController');
+const listReceiptsReadyToPayController = require('../src/modules/procurement/receipt/query/ready-to-pay/listReceiptsReadyToPayController');
 const listReceiptBarcodeSummariesController = require('../src/modules/procurement/receipt/barcode/summary/listReceiptBarcodeSummariesController');
 const generateReceiptBarcodesController = require('../src/modules/procurement/receipt/barcode/generate/generateReceiptBarcodesController');
 const markReceiptPrintedController = require('../src/modules/procurement/receipt/barcode/printed/markReceiptPrintedController');
 const printReceiptController = require('../src/modules/procurement/receipt/barcode/print/printReceiptController');
 const updateReceiptItemController = require('../src/modules/procurement/receipt/item/update/updateReceiptItemController');
+const finalizeReceiptController = require('../src/modules/procurement/receipt/finalize/finalizeReceiptController');
+const commitReceiptController = require('../src/modules/procurement/receipt/commit/commitReceiptController');
 
 const verifyToken = require('../middlewares/verifyToken');
 router.use(verifyToken);
 
 router.post('/', createPurchaseReceiptController.handle);
 router.get('/', listPurchaseReceiptsController.handle);
-router.get('/ready-to-pay', getReceiptsReadyToPay);
+router.get('/ready-to-pay', listReceiptsReadyToPayController.handle);
 router.get('/with-barcode-status', listReceiptBarcodeSummariesController.handle);
 router.get('/summaries', listReceiptBarcodeSummariesController.handle);
 router.get('/receipt-barcode-summaries', listReceiptBarcodeSummariesController.handle);
@@ -46,11 +43,11 @@ router.patch('/:receiptId/items/:itemId', (req, res) => {
 
 router.put('/:id', updatePurchaseReceiptNoteController.handle);
 router.delete('/:id', deletePurchaseReceiptController.handle);
-router.post('/:id/finalize', finalizeReceiptController);
-router.patch('/:id/finalize', finalizeReceiptController);
+router.post('/:id/finalize', finalizeReceiptController.handle);
+router.patch('/:id/finalize', finalizeReceiptController.handle);
 router.patch('/:id/printed', markReceiptPrintedController.handle);
 router.post('/:id/generate-barcodes', generateReceiptBarcodesController.handle);
 router.post('/:id/print', printReceiptController.handle);
-router.post('/:id/commit', commitReceipt);
+router.post('/:id/commit', commitReceiptController.handle);
 
 module.exports = router;
