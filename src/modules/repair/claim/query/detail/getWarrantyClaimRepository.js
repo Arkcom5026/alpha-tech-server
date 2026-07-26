658ed4c0a366ece928e179b1982078a17f35aff1
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 02:04:50 +0700

    refactor(repair): add warranty claim detail repository slice

---
+++ src/modules/repair/claim/query/detail/getWarrantyClaimRepository.js
@@ -0,0 +1,68 @@
+function getDefaultPrismaClient() {
+  return require('../../../../../database/prisma/client');
+}
+
+const warrantyClaimDetailInclude = {
+  branch: true,
+  stockItem: {
+    include: {
+      product: {
+        include: {
+          brand: true,
+          productType: true,
+        },
+      },
+    },
+  },
+  supplier: true,
+  repairJob: {
+    include: {
+      customer: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  },
+  previousClaim: true,
+  subsequentClaims: true,
+  replacementStockItem: {
+    include: {
+      product: true,
+    },
+  },
+  createdBy: true,
+  resolvedBy: true,
+  events: {
+    include: {
+      performedBy: true,
+    },
+    orderBy: {
+      occurredAt: 'asc',
+    },
+  },
+};
+
+class GetWarrantyClaimRepository {
+  constructor(client = null) {
+    this.client = client;
+  }
+
+  get prisma() {
+    return this.client || getDefaultPrismaClient();
+  }
+
+  findById(branchId, warrantyClaimId) {
+    return this.prisma.warrantyClaim.findFirst({
+      where: {
+        id: Number(warrantyClaimId),
+        branchId: Number(branchId),
+      },
+      include: warrantyClaimDetailInclude,
+    });
+  }
+}
+
+module.exports = new GetWarrantyClaimRepository();
+module.exports.GetWarrantyClaimRepository = GetWarrantyClaimRepository;
+module.exports.warrantyClaimDetailInclude = warrantyClaimDetailInclude;
