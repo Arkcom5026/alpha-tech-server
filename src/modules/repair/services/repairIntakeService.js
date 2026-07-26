b8ddb6616b06a62bf8da22bcc842b7ecbe97a2bb
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 00:59:08 +0700

    refactor(repair): route legacy intake service to slice

--- src/modules/repair/services/repairIntakeService.js
+++ src/modules/repair/services/repairIntakeService.js
@@ -1,35 +1,7 @@
-const repairRepository = require('../repositories/repairRepository');
-const { validateLookup } = require('../validators/repairValidator');
-const { mapIntakeContext } = require('../mappers/repairIntakeMapper');
+const intakeContextService = require('../query/intake-context/intakeContextService');
 const {
-  RepairError,
-  RepairFailureCode,
-} = require('../contracts/repairError');
+  IntakeContextService,
+} = require('../query/intake-context/intakeContextService');
 
-class RepairIntakeService {
-  constructor(repository = repairRepository) {
-    this.repository = repository;
-  }
-
-  async getContext(actor, rawLookup) {
-    const lookup = validateLookup(rawLookup);
-    const stockItem = await this.repository.findStockItemForIntake(
-      actor.branchId,
-      lookup
-    );
-
-    if (!stockItem) {
-      throw new RepairError(
-        RepairFailureCode.STOCK_ITEM_NOT_FOUND,
-        'ไม่พบสินค้าจากบาร์โค้ดหรือหมายเลขซีเรียลในสาขานี้',
-        404,
-        { lookup }
-      );
-    }
-
-    return mapIntakeContext(stockItem);
-  }
-}
-
-module.exports = new RepairIntakeService();
-module.exports.RepairIntakeService = RepairIntakeService;
+module.exports = intakeContextService;
+module.exports.RepairIntakeService = IntakeContextService;
