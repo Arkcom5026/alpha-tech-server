2846d18a91359f8c5a921e9f385f90d00183301c
Arkcom5026 <advicebanphot@gmail.com>
2026-07-26 13:22:44 +0700

    feat(tax): establish tax module public boundary

---
+++ src/modules/tax/index.js
@@ -0,0 +1,14 @@
+const contracts = require('./contracts');
+const policies = require('./policies');
+const shared = require('./shared');
+
+module.exports = {
+  contracts,
+  policies,
+  shared,
+  controllers: {},
+  services: {},
+  repositories: {},
+  projections: {},
+  routes: {},
+};
