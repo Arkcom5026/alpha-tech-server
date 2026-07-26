036f41ae596bcd86df728c3916e930e8e8d10fa4
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 02:49:33 +0700

    refactor(repair): remove legacy warranty asset service

--- src/modules/repair/services/customerWarrantyAssetService.js
+++
@@ -1,7 +0,0 @@
-const customerWarrantyAssetsService = require('../query/customer-warranty-assets/customerWarrantyAssetsService');
-
-module.exports = customerWarrantyAssetsService;
-module.exports.CustomerWarrantyAssetService =
-  customerWarrantyAssetsService.CustomerWarrantyAssetsService;
-module.exports.CustomerWarrantyAssetsService =
-  customerWarrantyAssetsService.CustomerWarrantyAssetsService;
