8d69f243217ffb3a4e3dc830d8eace538fd3927c
Arkcom5026 <advicebanphot@gmail.com>
2026-07-26 18:20:38 +0700

    feat(repair): add management alert dashboard route

--- src/modules/repair/routes/repairRoutes.js
+++ src/modules/repair/routes/repairRoutes.js
@@ -19,6 +19,7 @@ router.get('/customers/:customerId/warranty-assets', allowRepairRoles(...READ_AN
 router.get('/dashboard', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.getOperationalDashboard);
 router.get('/dashboard/risks', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.getOperationalRiskDashboard);
 router.get('/dashboard/decisions', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.getOperationalDecisionDashboard);
+router.get('/dashboard/alerts', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.getManagementAlertDashboard);
 router.get('/jobs', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.listJobs);
 router.get('/jobs/:id', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.getJob);
 router.post('/jobs', allowRepairRoles(...READ_AND_INTAKE_ROLES), repairController.createJob);
