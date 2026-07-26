2ed3429b4372c2e07df8acf7026f30bca1bb20c3
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 01:45:02 +0700

    refactor(repair): add open warranty claim repository slice

---
+++ src/modules/repair/claim/open/openWarrantyClaimRepository.js
@@ -0,0 +1,106 @@
+const warrantyClaimDetailInclude = {
+  branch: true,
+  stockItem: {
+    include: {
+      product: {
+        include: {
+          brand: true,
+          productType: true,
+        },
+      },
+    },
+  },
+  supplier: true,
+  repairJob: {
+    include: {
+      customer: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  },
+  previousClaim: true,
+  subsequentClaims: true,
+  replacementStockItem: {
+    include: {
+      product: true,
+    },
+  },
+  createdBy: true,
+  resolvedBy: true,
+  events: {
+    include: {
+      performedBy: true,
+    },
+    orderBy: {
+      occurredAt: 'asc',
+    },
+  },
+};
+
+class OpenWarrantyClaimRepository {
+  constructor(client = null) {
+    this.prisma = client;
+  }
+
+  getClient() {
+    if (!this.prisma) {
+      this.prisma = require('../../../../../database/prisma/client');
+    }
+    return this.prisma;
+  }
+
+  transaction(work) {
+    return this.getClient().$transaction((tx) =>
+      work(new OpenWarrantyClaimRepository(tx))
+    );
+  }
+
+  findRepairJob(branchId, repairJobId) {
+    return this.getClient().repairJob.findFirst({
+      where: {
+        id: Number(repairJobId),
+        branchId: Number(branchId),
+      },
+      include: {
+        stockItem: {
+          include: {
+            purchaseOrderReceiptItem: {
+              include: {
+                receipt: {
+                  include: {
+                    supplier: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        warrantyClaims: true,
+      },
+    });
+  }
+
+  findSupplier(supplierId) {
+    return this.getClient().supplier.findUnique({
+      where: { id: Number(supplierId) },
+    });
+  }
+
+  createWarrantyClaim(data, initialEvent) {
+    return this.getClient().warrantyClaim.create({
+      data: {
+        ...data,
+        events: {
+          create: initialEvent,
+        },
+      },
+      include: warrantyClaimDetailInclude,
+    });
+  }
+}
+
+module.exports = new OpenWarrantyClaimRepository();
+module.exports.OpenWarrantyClaimRepository = OpenWarrantyClaimRepository;
+module.exports.warrantyClaimDetailInclude = warrantyClaimDetailInclude;
