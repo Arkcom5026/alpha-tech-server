1802d8e6f9c46bf0037e63c40497d393d7f16a95
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 00:58:50 +0700

    refactor(repair): add intake context slice service

---
+++ src/modules/repair/query/intake-context/intakeContextService.js
@@ -0,0 +1,36 @@
+const intakeContextRepository = require('./intakeContextRepository');
+const { validateLookup } = require('../../validators/repairValidator');
+const { mapIntakeContext } = require('../../mappers/repairIntakeMapper');
+const {
+  RepairError,
+  RepairFailureCode,
+} = require('../../contracts/repairError');
+
+class IntakeContextService {
+  constructor(repository = intakeContextRepository) {
+    this.repository = repository;
+  }
+
+  async execute(actor, rawLookup) {
+    const lookup = validateLookup(rawLookup);
+    const stockItem = await this.repository.findByLookup(actor.branchId, lookup);
+
+    if (!stockItem) {
+      throw new RepairError(
+        RepairFailureCode.STOCK_ITEM_NOT_FOUND,
+        'ไม่พบสินค้าจากบาร์โค้ดหรือหมายเลขซีเรียลในสาขานี้',
+        404,
+        { lookup }
+      );
+    }
+
+    return mapIntakeContext(stockItem);
+  }
+
+  getContext(actor, rawLookup) {
+    return this.execute(actor, rawLookup);
+  }
+}
+
+module.exports = new IntakeContextService();
+module.exports.IntakeContextService = IntakeContextService;
