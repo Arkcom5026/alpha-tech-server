c56e20855a1e608d39aea99f4b37766dc2c94279
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 04:33:11 +0700

    feat(tax): expose tax period administrative routes

---
+++ src/modules/tax/periods/taxPeriodRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const verifyToken = require('../../../../middlewares/verifyToken');
+const controller = require('./taxPeriodController');
+
+const router = express.Router();
+router.use(verifyToken);
+
+router.get('/periods', controller.listPeriods);
+router.get('/periods/summary', controller.getPeriodSummary);
+router.get('/periods/:taxPeriodId', controller.getPeriodDetail);
+router.post('/periods/ensure', controller.ensureMonthlyPeriod);
+router.post('/periods/:taxPeriodId/close', controller.closePeriod);
+router.post('/periods/:taxPeriodId/lock', controller.lockPeriod);
+router.post('/periods/:taxPeriodId/submit', controller.submitPeriod);
+router.post('/periods/:taxPeriodId/reopen', controller.reopenPeriod);
+
+module.exports = router;
