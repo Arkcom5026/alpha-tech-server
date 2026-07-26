26f80a47b3add1f52ea4e1d634dd5b0de8926628
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 03:43:52 +0700

    feat(procurement): add receipt purchase order detail repository

---
+++ src/modules/procurement/receipt/query/purchase-order-detail/getReceiptPurchaseOrderRepository.js
@@ -0,0 +1,34 @@
+const { prisma } = require('../../../../../../lib/prisma');
+
+const receiptPurchaseOrderInclude = {
+  supplier: true,
+  items: {
+    include: {
+      product: {
+        include: {
+          productType: true,
+          brand: true,
+          unit: true,
+        },
+      },
+      receiptItems: true,
+    },
+  },
+};
+
+class GetReceiptPurchaseOrderRepository {
+  constructor(client = prisma) {
+    this.client = client;
+  }
+
+  findByIdAndBranch(id, branchId) {
+    return this.client.purchaseOrder.findFirst({
+      where: { id, branchId },
+      include: receiptPurchaseOrderInclude,
+    });
+  }
+}
+
+module.exports = new GetReceiptPurchaseOrderRepository();
+module.exports.GetReceiptPurchaseOrderRepository = GetReceiptPurchaseOrderRepository;
+module.exports.receiptPurchaseOrderInclude = receiptPurchaseOrderInclude;
