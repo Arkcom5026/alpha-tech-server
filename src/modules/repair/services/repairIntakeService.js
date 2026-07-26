9f79c78c25937ee803fed9e6f281e58adaa64d60
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 02:49:20 +0700

    refactor(repair): remove legacy intake service

--- src/modules/repair/services/repairIntakeService.js
+++
@@ -1,7 +0,0 @@
-const intakeContextService = require('../query/intake-context/intakeContextService');
-const {
-  IntakeContextService,
-} = require('../query/intake-context/intakeContextService');
-
-module.exports = intakeContextService;
-module.exports.RepairIntakeService = IntakeContextService;
