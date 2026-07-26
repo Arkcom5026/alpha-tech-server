2b7d5ef5c6bc018d74e75ff89c7f067bb2f0f61b
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 02:58:30 +0700

    feat(tax): add tax period detail administrative route

--- src/modules/tax/routes/taxPeriodAdministrativeRoutes.js
+++ src/modules/tax/routes/taxPeriodAdministrativeRoutes.js
@@ -29,6 +29,7 @@ const requireTaxPeriodAdministrator = (req, res, next) => {
 router.use(verifyToken, requireTaxPeriodAdministrator);
 
 router.get('/periods', controller.listPeriods);
+router.get('/periods/:taxPeriodId', controller.getPeriodDetail);
 router.post('/periods/ensure', controller.ensureMonthlyPeriod);
 router.post('/periods/readiness', controller.ensureOperationalReadiness);
 router.post('/periods/:taxPeriodId/close', controller.closePeriod);
