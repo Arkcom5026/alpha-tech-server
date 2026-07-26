59dfb5fb5a058416b9efa047fc165b8f2fb93603
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 01:32:32 +0700

    feat(repair): add update job status repository slice

---
+++ src/modules/repair/status/updateRepairJobStatusRepository.js
@@ -0,0 +1,69 @@
+const prisma = require('../../../database/prisma/client');
+
+const repairJobDetailInclude = {
+  branch: true,
+  customer: { include: { user: true } },
+  stockItem: {
+    include: {
+      product: { include: { brand: true, productType: true } },
+      purchaseOrderReceiptItem: {
+        include: { receipt: { include: { supplier: true } } },
+      },
+      saleItems: {
+        include: { sale: { include: { customer: { include: { user: true } } } } },
+        orderBy: { sale: { soldAt: 'desc' } },
+      },
+    },
+  },
+  technician: true,
+  partsUsed: { include: { product: true } },
+  warrantyClaims: {
+    include: {
+      supplier: true,
+      events: {
+        include: { performedBy: true },
+        orderBy: { occurredAt: 'asc' },
+      },
+    },
+    orderBy: { openedAt: 'desc' },
+  },
+};
+
+class UpdateRepairJobStatusRepository {
+  constructor(client = prisma) {
+    this.prisma = client;
+  }
+
+  transaction(work) {
+    return this.prisma.$transaction((tx) =>
+      work(new UpdateRepairJobStatusRepository(tx))
+    );
+  }
+
+  findJob(branchId, repairJobId) {
+    return this.prisma.repairJob.findFirst({
+      where: {
+        id: Number(repairJobId),
+        branchId: Number(branchId),
+      },
+      include: repairJobDetailInclude,
+    });
+  }
+
+  findTechnician(technicianId) {
+    return this.prisma.employeeProfile.findUnique({
+      where: { id: Number(technicianId) },
+    });
+  }
+
+  updateJob(repairJobId, data) {
+    return this.prisma.repairJob.update({
+      where: { id: Number(repairJobId) },
+      data,
+      include: repairJobDetailInclude,
+    });
+  }
+}
+
+module.exports = new UpdateRepairJobStatusRepository();
+module.exports.UpdateRepairJobStatusRepository = UpdateRepairJobStatusRepository;
