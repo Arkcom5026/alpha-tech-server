26afeed07e467de87b21553b1ac83371a2743bb9
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 02:49:00 +0700

    refactor(repair): remove legacy module exports

--- src/modules/repair/index.js
+++ src/modules/repair/index.js
@@ -1,9 +1,4 @@
 module.exports = {
   routes: require('./routes/repairRoutes'),
-  repairController: require('./controllers/repairController'),
-  repairService: require('./services/repairService'),
-  repairIntakeService: require('./services/repairIntakeService'),
-  warrantyClaimService: require('./services/warrantyClaimService'),
-  repairRepository: require('./repositories/repairRepository'),
   contracts: require('./contracts'),
 };
