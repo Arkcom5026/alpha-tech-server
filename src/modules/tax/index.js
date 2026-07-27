dcc86a3acea878afcab46cac61ba2c66de22fc9e
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 09:22:10 +0700

    feat(tax): expose intake foundation capabilities

--- src/modules/tax/index.js
+++ src/modules/tax/index.js
@@ -7,6 +7,14 @@
  * depend on them. Internal repositories remain private to their slices.
  */
 module.exports = Object.freeze({
+  candidates: Object.freeze({
+    contracts: require('./candidates/contracts/taxCandidateContract'),
+    mapping: require('./candidates/mapping/mapCandidateToTaxDocument'),
+  }),
+  documents: Object.freeze({
+    contracts: require('./documents/contracts/taxDocumentContract'),
+    lifecycle: require('./documents/lifecycle/taxDocumentLifecycle'),
+  }),
   periods: Object.freeze({
     routes: require('./periods/taxPeriodRoutes'),
     service: require('./periods/taxPeriodService'),
