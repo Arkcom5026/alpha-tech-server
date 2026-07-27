5a05cedbb337dcd5e2602c35cc3e1a650bb93647
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 10:07:56 +0700

    refactor(inventory): route stock audit finalize through module

--- src/modules/inventory/audit/routes/stockAuditRoutes.js
+++ src/modules/inventory/audit/routes/stockAuditRoutes.js
@@ -5,7 +5,7 @@ const { getActiveReadySession } = require('../query/active/getActiveAuditControl
 const { getOverview } = require('../query/overview/getAuditOverviewController');
 const { listAuditItems } = require('../query/items/listAuditItemsController');
 const { scanBarcodeController, scanSerialController } = require('../scan/scanAuditController');
-const { confirmAudit, cancelAudit } = require('../../../../../controllers/stockAuditController');
+const { confirmAuditController, cancelAuditController } = require('../finalize/finalizeAuditController');
 
 const router = express.Router();
 router.use(verifyToken);
@@ -15,8 +15,8 @@ router.post('/ready/start', startReadyAudit);
 router.get('/:sessionId/overview', getOverview);
 router.post('/:sessionId/scan', scanBarcodeController);
 router.post('/:sessionId/scan-sn', scanSerialController);
-router.post('/:sessionId/confirm', confirmAudit);
-router.post('/:sessionId/cancel', cancelAudit);
+router.post('/:sessionId/confirm', confirmAuditController);
+router.post('/:sessionId/cancel', cancelAuditController);
 router.get('/:sessionId/items', listAuditItems);
 
 module.exports = router;
