2248fc6685da7067d3b94e929adcfbdb506815cf
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 09:52:52 +0700

    feat(repair): add transactional workflow command repository

---
+++ src/modules/repair/workflow/commands/transitionRepairWorkflowRepository.js
@@ -0,0 +1,47 @@
+const prisma = require('../../../../database/prisma/client');
+const {
+  publishDevicePassportEvent,
+} = require('../../../device/passport/publish/devicePassportEventPublisher');
+
+const repairWorkflowInclude = {
+  device: true,
+  passportEvents: {
+    where: { sourceType: 'REPAIR_JOB' },
+    orderBy: [{ occurredAt: 'desc' }, { id: 'desc' }],
+    take: 1,
+  },
+};
+
+class TransitionRepairWorkflowRepository {
+  constructor(client = prisma) {
+    this.prisma = client;
+  }
+
+  transaction(work) {
+    return this.prisma.$transaction((tx) =>
+      work(new TransitionRepairWorkflowRepository(tx))
+    );
+  }
+
+  findRepairJob(repairJobId) {
+    return this.prisma.repairJob.findUnique({
+      where: { id: Number(repairJobId) },
+      include: repairWorkflowInclude,
+    });
+  }
+
+  updateLegacyStatus(repairJobId, status) {
+    return this.prisma.repairJob.update({
+      where: { id: Number(repairJobId) },
+      data: { status },
+      include: repairWorkflowInclude,
+    });
+  }
+
+  publishPassportEvent(event) {
+    return publishDevicePassportEvent(this.prisma, event);
+  }
+}
+
+module.exports = new TransitionRepairWorkflowRepository();
+module.exports.TransitionRepairWorkflowRepository = TransitionRepairWorkflowRepository;
