0c32e7807cf66a67435f1bbf0cd42036d2299e39
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 09:46:18 +0700

    test(tax): cover intake HTTP boundary contract

---
+++ tests/tax-intake-http.contract.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('assert');
+const controller = require('../src/modules/tax/http/taxIntakeController');
+const routes = require('../src/modules/tax/http/taxIntakeRoutes');
+const tax = require('../src/modules/tax');
+
+assert.strictEqual(typeof controller.registerCandidate, 'function');
+assert.strictEqual(typeof controller.listCandidates, 'function');
+assert.strictEqual(typeof controller.listDocuments, 'function');
+assert.strictEqual(typeof controller.getDocumentDetail, 'function');
+
+const routeContracts = routes.stack
+  .filter((layer) => layer.route)
+  .map((layer) => ({
+    path: layer.route.path,
+    methods: Object.keys(layer.route.methods).sort(),
+  }));
+
+assert.deepStrictEqual(routeContracts, [
+  { path: '/candidates/register', methods: ['post'] },
+  { path: '/candidates', methods: ['get'] },
+  { path: '/documents', methods: ['get'] },
+  { path: '/documents/:taxDocumentId', methods: ['get'] },
+]);
+
+assert.strictEqual(tax.intake.routes, routes);
+assert.strictEqual(typeof tax.intake.service.registerTaxCandidate, 'function');
+assert.strictEqual(typeof tax.intake.service.listCandidates, 'function');
+assert.strictEqual(typeof tax.intake.service.listDocuments, 'function');
+assert.strictEqual(typeof tax.intake.service.getDocumentDetail, 'function');
+
+console.log('Tax intake HTTP contract: PASS');
