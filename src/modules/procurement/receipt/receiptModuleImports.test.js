7658efb2facc4485457c3abace9dfc017149ab3d
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 15:35:58 +0700

    test(procurement): guard purchase receipt startup imports

---
+++ src/modules/procurement/receipt/receiptModuleImports.test.js
@@ -0,0 +1,10 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+test('purchase receipt route graph resolves at server startup', () => {
+  const receiptRoutes = require('../../../../routes/purchaseOrderReceiptRoutes');
+  const receiptItemRoutes = require('../../../../routes/purchaseOrderReceiptItemRoutes');
+
+  assert.equal(typeof receiptRoutes, 'function');
+  assert.equal(typeof receiptItemRoutes, 'function');
+});
