052cf69a5e6d3385b1735a378a0658dc09a6b019
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 05:38:28 +0700

    feat(tax): expose output tax period reporting

--- src/modules/tax/http/taxIntakeService.js
+++ src/modules/tax/http/taxIntakeService.js
@@ -16,6 +16,7 @@ const {
 } = require('../documents/readiness/buildTaxDocumentOperationalReadinessProjection');
 const { buildTaxDocumentTimelineProjection } = require('../documents/timeline/buildTaxDocumentTimelineProjection');
 const { buildTaxDocumentWorkspaceProjection } = require('../documents/workspace/buildTaxDocumentWorkspaceProjection');
+const { buildOutputTaxPeriodReport } = require('../outputTax/reporting/buildOutputTaxPeriodReportService');
 const { registerTaxCandidate } = require('../intake/registerTaxCandidateService');
 const { registerSaleTaxCandidate } = require('../sources/sale/registerSaleTaxCandidateService');
 const {
@@ -102,6 +103,12 @@ const getDocumentWorkspaceProjection = async (input) => {
   return buildTaxDocumentWorkspaceProjection({ document });
 };
 
+const getOutputTaxPeriodReport = (input) => buildOutputTaxPeriodReport({
+  branchId: requirePositiveInt(input.branchId, 'TAX_BRANCH_REQUIRED', 'branchId'),
+  year: input.year,
+  month: input.month,
+});
+
 module.exports = Object.freeze({
   cancelTaxDocument,
   convertTaxCandidate,
@@ -111,6 +118,7 @@ module.exports = Object.freeze({
   getDocumentReplacementChainProjection,
   getDocumentTimelineProjection,
   getDocumentWorkspaceProjection,
+  getOutputTaxPeriodReport,
   issueTaxDocument,
   listCandidates,
   listDocuments,
