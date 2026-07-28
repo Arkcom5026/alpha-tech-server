add3baf6b0bfe8c0c399f48eef1a2cceea712186
Arkcom5026 <advicebanphot@gmail.com>
2026-07-28 17:19:25 +0700

    feat(reservation): expose reservation query and cancel routes

--- src/modules/sales/reservation/routes/productReservationRoutes.js
+++ src/modules/sales/reservation/routes/productReservationRoutes.js
@@ -2,8 +2,16 @@
 
 const express = require('express');
 const { createProductReservationController } = require('../create/productReservationCreateController');
+const {
+  listProductReservationsController,
+  getProductReservationByIdController,
+} = require('../query/productReservationQueryController');
+const { cancelProductReservationController } = require('../cancel/productReservationCancelController');
 
 const router = express.Router();
+router.get('/', listProductReservationsController);
+router.get('/:id', getProductReservationByIdController);
 router.post('/', createProductReservationController);
+router.post('/:id/cancel', cancelProductReservationController);
 
 module.exports = router;
