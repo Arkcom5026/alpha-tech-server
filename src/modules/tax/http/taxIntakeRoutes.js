57c317666366e09837745d7144db9ce44e5ab271
Arkcom5026 <advicebanphot@gmail.com>
2026-07-28 22:55:48 +0700

    feat(tax): add issue and print projection routes

--- src/modules/tax/http/taxIntakeRoutes.js
+++ src/modules/tax/http/taxIntakeRoutes.js
@@ -16,7 +16,9 @@ router.post('/candidates/register', controller.registerCandidate);
 router.post('/candidates/register-sale/:saleId', controller.registerSaleCandidate);
 router.get('/candidates', controller.listCandidates);
 router.get('/documents', controller.listDocuments);
+router.get('/documents/:taxDocumentId/print-projection', controller.getDocumentPrintProjection);
 router.get('/documents/:taxDocumentId', controller.getDocumentDetail);
+router.post('/documents/:taxDocumentId/issue', controller.issueDocument);
 router.post('/documents/:taxDocumentId/transition', controller.transitionDocument);
 
 module.exports = router;
