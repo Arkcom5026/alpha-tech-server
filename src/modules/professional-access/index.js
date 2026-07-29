3deef0ecd788c1a52b3242ad3d8c1059d71e55f9
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 22:57:12 +0700

    feat(professional-access): expose public contracts and authority

--- src/modules/professional-access/index.js
+++ src/modules/professional-access/index.js
@@ -1,10 +1,14 @@
 const professionalAccessRoutes = require('./routes/professionalAccessRoutes');
+const professionalAccessContract = require('./contracts/professionalAccess.contract');
+const professionalAccessAuthority = require('./shared/professionalAccessAuthority');
 
 const mountProfessionalAccessModule = (app) => {
-  app.use('/api/professional-access', professionalAccessRoutes);
+  app.use(professionalAccessContract.PROFESSIONAL_ACCESS_BASE_PATH, professionalAccessRoutes);
 };
 
 module.exports = {
   mountProfessionalAccessModule,
+  professionalAccessAuthority,
+  professionalAccessContract,
   professionalAccessRoutes,
 };
