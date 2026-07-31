d14b145ec650bbb1d9da9f82e90a86a07a44f821
Arkcom5026 <advicebanphot@gmail.com>
2026-08-01 05:54:19 +0700

    refactor(auth): route runtime controller through session service

--- src/modules/auth/session/runtime/sessionAuthRuntimeController.js
+++ src/modules/auth/session/runtime/sessionAuthRuntimeController.js
@@ -1,1 +1,13 @@
-module.exports = require('../../../../../controllers/authController');
+const service = require('./sessionAuthRuntimeService');
+
+module.exports = {
+  login: service.login,
+  register: service.register,
+  refreshSession: service.refreshSession,
+  logoutSession: service.logoutSession,
+  getMe: service.getMe,
+  forgotPassword: service.forgotPassword,
+  resetPassword: service.resetPassword,
+  findUserByEmail: service.findUserByEmail,
+  revokeSession: service.revokeSession,
+};
