f86492c9b124bdaddfab6fc40e8b0a6491fe49ce
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 21:49:24 +0700

    Route customer creation through vertical slice

--- src/modules/customer/routes/customerRoutes.js
+++ src/modules/customer/routes/customerRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const customerByPhoneController = require('../query/by-phone/customerByPhoneController');
 const customerByNameController = require('../query/by-name/customerByNameController');
 const customerSelfController = require('../query/self/customerSelfController');
-const { createCustomer } = require('../controllers/customerCreateController');
+const customerCreateController = require('../create/customerCreateController');
 const {
   updateCustomerProfile,
   updateCustomerProfileOnline,
@@ -17,7 +17,7 @@ router.get('/by-phone/:phone', customerByPhoneController.getCustomerByPhone);
 router.get('/by-name', customerByNameController.getCustomerByName);
 router.get('/me', customerSelfController.getCustomerSelf);
 
-router.post('/', createCustomer);
+router.post('/', customerCreateController.createCustomer);
 router.put('/me', updateCustomerProfileOnline);
 router.put('/:id', updateCustomerProfile);
 
