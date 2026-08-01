3ea9d773d34d7cf52901ebedfdf3d37518de927f
Arkcom5026 <advicebanphot@gmail.com>
2026-08-01 12:19:11 +0700

    fix(employee): align onboarding controller export contract

--- src/modules/employee/onboarding/runtime/employeeOnboardingRuntimeController.js
+++ src/modules/employee/onboarding/runtime/employeeOnboardingRuntimeController.js
@@ -1,7 +1,5 @@
 const service = require('./employeeOnboardingRuntimeService');
 
-const addSubEmployee = async (req, res) => service.addSubEmployee(req, res);
-
 module.exports = {
-  addSubEmployee,
+  addSubEmployee: service.addSubEmployee,
 };
