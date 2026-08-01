f46448d9faf53d94faab0e8504d42f39a9e46f47
Arkcom5026 <advicebanphot@gmail.com>
2026-08-01 10:34:22 +0700

    refactor(position): cut routes over to runtime controller

--- src/modules/position/routes/positionRoutes.js
+++ src/modules/position/routes/positionRoutes.js
@@ -10,27 +10,20 @@ const {
   updatePosition,
   toggleActive,
   hardDelete,
-} = require('../controllers/positionController');
+} = require('../runtime/positionRuntimeController');
 
 const verifyToken = require('../../../../middlewares/verifyToken');
 const requireAdmin = require('../../../../middlewares/requireAdmin');
 
-// All routes require authentication.
 router.use(verifyToken);
 
-// Keep static routes before dynamic /:id routes.
 router.get('/dropdowns', getDropdowns);
-
-// Read operations are available to every authenticated user.
 router.get('/', listPositions);
 router.get('/:id', getById);
 
-// Write operations require admin authority.
 router.post('/', requireAdmin, createPosition);
 router.patch('/:id', requireAdmin, updatePosition);
 router.patch('/:id/toggle-active', requireAdmin, toggleActive);
-
-// Hard delete remains protected by admin authority.
 router.delete('/:id', requireAdmin, hardDelete);
 
 module.exports = router;
