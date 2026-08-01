36a37bcaf168595d5790e9afc854ebabc9ee832a
Arkcom5026 <advicebanphot@gmail.com>
2026-08-01 10:57:08 +0700

    refactor(brand): cut routes over to runtime controller

--- src/modules/brand/routes/brandRoutes.js
+++ src/modules/brand/routes/brandRoutes.js
@@ -16,24 +16,16 @@ const {
   listProductTypeBrands,
   attachBrandToProductType,
   detachBrandFromProductType,
-} = require('../controllers/brandController');
+} = require('../runtime/brandRuntimeController');
 
 router.use(verifyToken);
 
-// Brand-owned options for Brand page.
-// This endpoint exists to keep Brand FE self-contained and avoid importing ProductType FE store/api.
 router.get('/product-type-options', listProductTypeOptions);
-
-// Brand read/dropdown
 router.get('/dropdowns', listBrandDropdowns);
 router.get('/', listBrands);
-
-// Brand master data
 router.post('/', createBrand);
 router.put('/:id', updateBrand);
 router.patch('/:id/toggle', toggleBrand);
-
-// ProductTypeBrand mapping under Brand module
 router.get('/product-type-brands', listProductTypeBrands);
 router.post('/product-type-brands', attachBrandToProductType);
 router.delete('/product-type-brands/:id', detachBrandFromProductType);
