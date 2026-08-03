026264bc872aa989ce9dc80e7b21c932f3294d6a
Arkcom5026 <advicebanphot@gmail.com>
2026-08-03 09:47:06 +0700

    feat(tax): expose issuer profile configuration route

--- src/modules/tax/http/taxIntakeRoutes.js
+++ src/modules/tax/http/taxIntakeRoutes.js
@@ -6,6 +6,7 @@ const controller = require('./taxIntakeController');
 const pendingInputTaxDocumentRoutes = require('../inputDocuments/pending/pendingInputTaxDocumentRoutes');
 const inputTaxReceiptLinkRoutes = require('../inputDocuments/links/inputTaxReceiptLinkRoutes');
 const inputTaxOverviewRoutes = require('../inputDocuments/overview/inputTaxOverviewRoutes');
+const taxIssuerProfileRoutes = require('../issuerProfile/routes/taxIssuerProfileRoutes');
 
 const router = express.Router();
 router.use(verifyToken);
@@ -13,6 +14,7 @@ router.use(verifyToken);
 router.use('/input-documents/overview', inputTaxOverviewRoutes);
 router.use('/input-documents/pending', pendingInputTaxDocumentRoutes);
 router.use('/documents/:taxDocumentId/receipt-links', inputTaxReceiptLinkRoutes);
+router.use('/issuer-profile', taxIssuerProfileRoutes);
 
 router.post('/candidates/register', controller.registerCandidate);
 router.post('/candidates/register-sale/:saleId', controller.registerSaleCandidate);
