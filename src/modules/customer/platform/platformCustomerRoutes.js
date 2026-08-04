a12ba596956a15a232a52874f8c5317f5888e496
Arkcom5026 <advicebanphot@gmail.com>
2026-08-04 16:47:35 +0700

    refactor(customer): remove unused platform route wrapper

--- src/modules/customer/platform/platformCustomerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const verifyToken = require('../../../middlewares/verifyToken');
-const controller = require('./overview/platformCustomerOverviewController');
-
-const router = express.Router();
-router.use(verifyToken);
-router.get('/overview', controller.getOverview);
-
-module.exports = router;
