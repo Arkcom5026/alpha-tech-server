6d438940507f235b56dd700c2f9d7d7887cd9e80
Arkcom5026 <advicebanphot@gmail.com>
2026-08-07 22:37:40 +0700

    feat(store-device): expose sale receipt print job controller

---
+++ src/modules/storeDevice/print/createSaleReceiptPrintJobController.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const {
+  createSaleReceiptPrintJobService,
+} = require('./createSaleReceiptPrintJobService')
+
+const service = createSaleReceiptPrintJobService()
+
+const createSaleReceiptPrintJob = async (req, res) => {
+  try {
+    const data = await service.execute({
+      user: req.user,
+      paymentId: req.params.paymentId,
+      payload: req.body || {},
+    })
+
+    return res.status(201).json({ data })
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({
+      error: error.message,
+      code: error.code || 'STORE_DEVICE_PRINT_JOB_FAILED',
+    })
+  }
+}
+
+module.exports = {
+  createSaleReceiptPrintJob,
+}
