818c21e2dbd1fac52cff637df173ae372b4ba01e
Arkcom5026 <advicebanphot@gmail.com>
2026-08-07 19:31:31 +0700

    feat(document-purpose): add system bootstrap repository

---
+++ src/modules/document-purpose/bootstrap/systemDocumentPurposeBootstrapRepository.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const { prisma, Prisma } = require('../../../lib/prisma')
+
+const isKnownRequestError = (error, code) =>
+  error instanceof Prisma.PrismaClientKnownRequestError && error.code === code
+
+class SystemDocumentPurposeBootstrapRepository {
+  constructor(client = prisma) {
+    this.prisma = client
+  }
+
+  branchExists(branchId) {
+    return this.prisma.branch.findUnique({
+      where: { id: Number(branchId) },
+      select: { id: true },
+    })
+  }
+
+  findByNormalizedCodes(branchId, normalizedCodes) {
+    return this.prisma.documentPurposeDefinition.findMany({
+      where: {
+        branchId: Number(branchId),
+        normalizedCode: { in: normalizedCodes },
+      },
+      select: {
+        id: true,
+        branchId: true,
+        code: true,
+        normalizedCode: true,
+        displayName: true,
+        description: true,
+        categoryCode: true,
+        isSystem: true,
+        lifecycleState: true,
+        sortOrder: true,
+        metadata: true,
+        currentVersion: true,
+      },
+    })
+  }
+
+  transaction(work) {
+    return this.prisma.$transaction((tx) => work(new SystemDocumentPurposeBootstrapRepository(tx)))
+  }
+
+  createDefinition(data) {
+    return this.prisma.documentPurposeDefinition.create({ data })
+  }
+
+  createVersion(data) {
+    return this.prisma.documentPurposeVersion.create({ data })
+  }
+
+  createEvent(data) {
+    return this.prisma.documentPurposeEvent.create({ data })
+  }
+}
+
+module.exports = {
+  SystemDocumentPurposeBootstrapRepository,
+  isKnownRequestError,
+}
