4f608c14723bade6fa6b4f3049ee51d1cdddffde
Arkcom5026 <advicebanphot@gmail.com>
2026-08-08 21:33:50 +0700

    wire customer money receive route export

--- src/modules/customer-money/receive/index.js
+++ src/modules/customer-money/receive/index.js
@@ -2,8 +2,10 @@
 
 const { receiveCustomerMoneyController } = require('./receiveCustomerMoneyController');
 const { receiveCustomerMoneyService } = require('./receiveCustomerMoneyService');
+const { createReceiveCustomerMoneyRoute } = require('./receiveCustomerMoneyRoute');
 
 module.exports = {
   receiveCustomerMoneyController,
   receiveCustomerMoneyService,
+  createReceiveCustomerMoneyRoute,
 };
