dafac686ebd0fc3011836e200b3bb27132b8a67b
Arkcom5026 <advicebanphot@gmail.com>
2026-08-08 21:30:28 +0700

    Add customer money receive route adapter

---
+++ src/modules/customer-money/receive/receiveCustomerMoneyRoute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const { receiveCustomerMoneyController } = require('./receiveCustomerMoneyController');
+
+const createReceiveCustomerMoneyRoute = ({ receiveCustomerMoneyService }) => {
+  const router = express.Router();
+
+  router.post('/customer-money/receive', (req, res, next) => {
+    req.receiveCustomerMoneyService = receiveCustomerMoneyService;
+    return receiveCustomerMoneyController(req, res, next);
+  });
+
+  return router;
+};
+
+module.exports = { createReceiveCustomerMoneyRoute };
