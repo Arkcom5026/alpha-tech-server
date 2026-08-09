4d9845e56e4edd2edc93fec41ba99d2af2036ca0
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 10:11:48 +0700

    feat(finance): add receive money read routes

--- src/modules/finance/customer-money-receive/routes/customerMoneyReceiveRoutes.js
+++ src/modules/finance/customer-money-receive/routes/customerMoneyReceiveRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { receiveCustomerMoney } = require('../receiveCustomerMoneyController');
+const {
+  receiveCustomerMoney,
+  listCustomerMoneyReceives,
+  getCustomerMoneyReceive,
+} = require('../receiveCustomerMoneyController');
 
 const verifyToken = require('../../../../../middlewares/verifyToken');
 const { traceVerifyToken } = require('../../../../../middlewares/authTrace');
 
 router.use(traceVerifyToken, verifyToken);
-
+router.get('/', listCustomerMoneyReceives);
+router.get('/:id', getCustomerMoneyReceive);
 router.post('/', receiveCustomerMoney);
 
 module.exports = router;
