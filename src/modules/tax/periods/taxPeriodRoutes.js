af03def16ba3b741e5c89555518f936bbef5f4af
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 20:45:55 +0700

    feat(tax): mount accounting office package under tax routes

--- src/modules/tax/periods/taxPeriodRoutes.js
+++ src/modules/tax/periods/taxPeriodRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const verifyToken = require('../../../../middlewares/verifyToken');
 const controller = require('./taxPeriodController');
+const accountingOfficeController = require('../accountingOffice/accountingOfficePackageController');
 
 const router = express.Router();
 router.use(verifyToken);
@@ -8,6 +9,7 @@ router.use(verifyToken);
 router.get('/periods', controller.listPeriods);
 router.get('/periods/summary', controller.getPeriodSummary);
 router.get('/periods/:taxPeriodId', controller.getPeriodDetail);
+router.get('/accounting-office/packages/:taxPeriodId', accountingOfficeController.getPackage);
 router.post('/periods/ensure', controller.ensureMonthlyPeriod);
 router.post('/periods/:taxPeriodId/close', controller.closePeriod);
 router.post('/periods/:taxPeriodId/lock', controller.lockPeriod);
