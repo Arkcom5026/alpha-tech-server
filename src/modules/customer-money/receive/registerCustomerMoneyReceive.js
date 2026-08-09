0db8f7f3cca0a694fe7cb5eed03b6536f8b0f229
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 10:24:42 +0700

    feat(customer-money): wire receive module runtime

--- src/modules/customer-money/receive/registerCustomerMoneyReceive.js
+++ src/modules/customer-money/receive/registerCustomerMoneyReceive.js
@@ -1,13 +1,52 @@
 'use strict';
 
-const { createCustomerMoneyReceiveRuntime } = require('./customerMoneyReceiveRuntime');
+const { prisma } = require('../../../lib/prisma');
+const verifyToken = require('../../../middlewares/verifyToken');
+const { createCustomerMoneyLedger } = require('../ledger/createCustomerMoneyLedgerService');
+const { updateCustomerMoneyBalance } = require('../balance/updateCustomerMoneyBalanceService');
+const {
+  createCustomerMoneyReceipt,
+  listCustomerMoneyReceipts,
+  getCustomerMoneyReceipt,
+} = require('./createCustomerMoneyReceiptRepository');
+const {
+  receiveCustomerMoney,
+  listCustomerMoneyReceives,
+  getCustomerMoneyReceive,
+} = require('./receiveCustomerMoneyService');
+const { createReceiveCustomerMoneyRoute } = require('./receiveCustomerMoneyRoute');
+
+const createRuntime = () => ({
+  receive: (input, user) => receiveCustomerMoney({
+    prisma,
+    receiptRepository: createCustomerMoneyReceipt,
+    createLedger: createCustomerMoneyLedger,
+    updateBalance: updateCustomerMoneyBalance,
+    input,
+    user,
+  }),
+  list: (user, query) => listCustomerMoneyReceives({
+    prisma,
+    listRepository: listCustomerMoneyReceipts,
+    user,
+    query,
+  }),
+  getById: (id, user) => getCustomerMoneyReceive({
+    prisma,
+    getRepository: getCustomerMoneyReceipt,
+    user,
+    id,
+  }),
+});
 
 const mountCustomerMoneyReceiveModule = (app) => {
-  const runtime = createCustomerMoneyReceiveRuntime();
-
-  app.use('/api/customer-money', runtime.routes);
-
-  return runtime;
+  const runtime = createRuntime();
+  const router = createReceiveCustomerMoneyRoute({ verifyToken, runtime });
+  app.use('/api/customer-money-receive', router);
+  return router;
 };
 
-module.exports = { mountCustomerMoneyReceiveModule };
+module.exports = {
+  createRuntime,
+  mountCustomerMoneyReceiveModule,
+};
