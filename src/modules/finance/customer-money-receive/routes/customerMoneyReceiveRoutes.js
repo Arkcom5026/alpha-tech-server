7015611b4763efc81a640944dd1f65adbd803ea3
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 10:25:58 +0700

    refactor(customer-money): restore duplicate finance route foundation

--- src/modules/finance/customer-money-receive/routes/customerMoneyReceiveRoutes.js
+++ src/modules/finance/customer-money-receive/routes/customerMoneyReceiveRoutes.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-  receiveCustomerMoney,
-  listCustomerMoneyReceives,
-  getCustomerMoneyReceive,
-} = require('../receiveCustomerMoneyController');
+const { receiveCustomerMoney } = require('../receiveCustomerMoneyController');
 
 const verifyToken = require('../../../../../middlewares/verifyToken');
 const { traceVerifyToken } = require('../../../../../middlewares/authTrace');
 
 router.use(traceVerifyToken, verifyToken);
-router.get('/', listCustomerMoneyReceives);
-router.get('/:id', getCustomerMoneyReceive);
+
 router.post('/', receiveCustomerMoney);
 
 module.exports = router;
