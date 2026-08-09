c5c1dca024319154715120ceb0a67ce0d7425fb6
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 10:25:52 +0700

    refactor(customer-money): restore duplicate finance controller foundation

--- src/modules/finance/customer-money-receive/receiveCustomerMoneyController.js
+++ src/modules/finance/customer-money-receive/receiveCustomerMoneyController.js
@@ -3,25 +3,11 @@ const customerMoneyReceiveService = require('./receiveCustomerMoneyService');
 async function receiveCustomerMoney(req, res, next) {
   try {
     const result = await customerMoneyReceiveService.receive(req.body, req.user);
-    return res.status(201).json({ ok: true, data: result });
-  } catch (error) {
-    next(error);
-  }
-}
 
-async function listCustomerMoneyReceives(req, res, next) {
-  try {
-    const result = await customerMoneyReceiveService.list(req.user, req.query);
-    return res.json({ ok: true, data: result });
-  } catch (error) {
-    next(error);
-  }
-}
-
-async function getCustomerMoneyReceive(req, res, next) {
-  try {
-    const result = await customerMoneyReceiveService.getById(req.params.id, req.user);
-    return res.json({ ok: true, data: result });
+    return res.status(201).json({
+      ok: true,
+      data: result,
+    });
   } catch (error) {
     next(error);
   }
@@ -29,6 +15,4 @@ async function getCustomerMoneyReceive(req, res, next) {
 
 module.exports = {
   receiveCustomerMoney,
-  listCustomerMoneyReceives,
-  getCustomerMoneyReceive,
 };
