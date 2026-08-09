58aa0e1bb622be7c51b3fb5b4a1a8ef58b5cf81e
Arkcom5026 <advicebanphot@gmail.com>
2026-08-09 13:34:57 +0700

    feat(customer-money): add settlement persistence repository

---
+++ src/modules/customer-money/settlement/delivery-credit/deliveryCreditSettlementRepository.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const settlementInclude = {
+  customer: {
+    select: { id: true, name: true, companyName: true, taxId: true },
+  },
+  lines: {
+    orderBy: [{ saleId: 'asc' }, { id: 'asc' }],
+    include: { application: true },
+  },
+};
+
+const createSettlement = ({ client, data }) => client.customerMoneySettlement.create({
+  data,
+  include: settlementInclude,
+});
+
+const getSettlement = ({ client, id, branchId }) => client.customerMoneySettlement.findFirst({
+  where: { id, branchId, settlementType: 'DELIVERY_CREDIT' },
+  include: settlementInclude,
+});
+
+const listSettlements = ({ client, branchId, customerId = null, take = 100 }) => client.customerMoneySettlement.findMany({
+  where: {
+    branchId,
+    settlementType: 'DELIVERY_CREDIT',
+    ...(customerId ? { customerId } : {}),
+  },
+  include: settlementInclude,
+  orderBy: [{ settledAt: 'desc' }, { id: 'desc' }],
+  take,
+});
+
+module.exports = { settlementInclude, createSettlement, getSettlement, listSettlements };
