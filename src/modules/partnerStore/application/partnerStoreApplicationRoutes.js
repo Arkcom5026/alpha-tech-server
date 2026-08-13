ee6db2dbf8dc281f885bab816d968e54f526f389
Arkcom5026 <advicebanphot@gmail.com>
2026-08-13 23:22:00 +0700

    refactor(partner-store): wire public intake authority

--- src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
+++ src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
@@ -3,16 +3,17 @@
 const express = require('express')
 const verifyToken = require('../../../../middlewares/verifyToken')
 const requireAdmin = require('../../../../middlewares/requireAdmin')
-const controller = require('./partnerStoreApplicationController')
+const publicController = require('./partnerStoreApplicationPublicController')
+const adminController = require('./partnerStoreApplicationController')
 
 const publicRouter = express.Router()
 const adminRouter = express.Router()
 
-publicRouter.post('/', controller.submit)
+publicRouter.post('/', publicController.submit)
 
 adminRouter.use(verifyToken, requireAdmin.superadmin)
-adminRouter.get('/', controller.list)
-adminRouter.post('/:id/approve', controller.approve)
-adminRouter.post('/:id/reject', controller.reject)
+adminRouter.get('/', adminController.list)
+adminRouter.post('/:id/approve', adminController.approve)
+adminRouter.post('/:id/reject', adminController.reject)
 
 module.exports = { publicRouter, adminRouter }
