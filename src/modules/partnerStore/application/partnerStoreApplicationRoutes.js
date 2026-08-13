55c282523719698af8d1d85714c6aa4f393a6542
Arkcom5026 <advicebanphot@gmail.com>
2026-08-13 22:43:25 +0700

    feat(partner-store): add application review transition route

--- src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
+++ src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
@@ -12,6 +12,7 @@ publicRouter.post('/', controller.submit)
 
 adminRouter.use(verifyToken, requireAdmin.superadmin)
 adminRouter.get('/', controller.list)
+adminRouter.post('/:id/review', controller.startReview)
 adminRouter.post('/:id/approve', controller.approve)
 adminRouter.post('/:id/reject', controller.reject)
 
