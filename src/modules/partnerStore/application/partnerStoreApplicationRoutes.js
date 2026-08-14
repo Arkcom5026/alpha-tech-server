bd4d9358ec392b7214426a9ff1b923898491b16d
Arkcom5026 <advicebanphot@gmail.com>
2026-08-14 09:36:25 +0700

    feat(partner-store): wire activation invitation route

--- src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
+++ src/modules/partnerStore/application/partnerStoreApplicationRoutes.js
@@ -5,6 +5,7 @@ const verifyToken = require('../../../../middlewares/verifyToken')
 const requireAdmin = require('../../../../middlewares/requireAdmin')
 const publicController = require('./partnerStoreApplicationPublicController')
 const adminController = require('./partnerStoreApplicationController')
+const activationController = require('./partnerStoreActivationController')
 
 const publicRouter = express.Router()
 const adminRouter = express.Router()
@@ -17,5 +18,6 @@ adminRouter.post('/:id/review', adminController.startReview)
 adminRouter.post('/:id/approve', adminController.approve)
 adminRouter.post('/:id/reject', adminController.reject)
 adminRouter.post('/:id/provision', adminController.provision)
+adminRouter.post('/:id/activation-invitations', activationController.issueInvitation)
 
 module.exports = { publicRouter, adminRouter }
