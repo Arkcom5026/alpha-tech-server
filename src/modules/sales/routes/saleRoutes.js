d608685e149fa38fb186c935d59ee57eea8d6b87
Arkcom5026 <advicebanphot@gmail.com>
2026-08-19 01:29:21 +0700

    feat(lineage): expose sale quotation reference route

--- src/modules/sales/routes/saleRoutes.js
+++ src/modules/sales/routes/saleRoutes.js
@@ -6,6 +6,7 @@ const { updateSaleDocumentLinesController } = require('../documents/controllers/
 const { getSaleDeliveryNote } = require('../documents/controllers/saleDeliveryNoteController');
 const { getAllSales, getAllSalesReturn, getSaleById, searchPrintableSales } = require('../history/controllers/saleHistoryController');
 const { searchSaleItemsController } = require('../item-search/controllers/saleItemSearchController');
+const { getSaleQuotationReferenceController } = require('../lineage/saleQuotationReferenceController');
 const { markSaleAsPaid } = require('../settlement/controllers/saleSettlementController');
 const saleReturnRoutes = require('../return/routes/saleReturnRoutes');
 const posHeldCartRoutes = require('../held-cart/routes/posHeldCartRoutes');
@@ -25,6 +26,7 @@ router.get('/printable', searchPrintableSales);
 router.get('/printable-sales', searchPrintableSales);
 router.put('/:id/document-lines', updateSaleDocumentLinesController);
 router.get('/:id/delivery-note', getSaleDeliveryNote);
+router.get('/:id/quotation-reference', getSaleQuotationReferenceController);
 router.put('/:id/document-descriptions', updateSaleDocumentLinesController);
 router.get('/:id', getSaleById);
 router.post('/:id/mark-paid', markSaleAsPaid);
