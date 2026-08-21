51b62ecdf0ebd7f7ca6d4935a69a91b46f03bb14
Arkcom5026 <advicebanphot@gmail.com>
2026-08-21 14:29:27 +0700

    feat(supplier): apply position-first master-data authority

--- src/modules/supplier/routes/supplierRoutes.js
+++ src/modules/supplier/routes/supplierRoutes.js
@@ -6,14 +6,18 @@ const listSuppliersController = require('../query/list/listSuppliersSlice');
 const getSupplierController = require('../query/detail/getSupplierSlice');
 const updateSupplierController = require('../update/updateSupplierSlice');
 const deleteSupplierController = require('../delete/deleteSupplierSlice');
+const {
+  allowSupplierAccess,
+  allowSupplierDelete,
+} = require('../shared/supplierAuthorization');
 
 const verifyToken = require('../../../../middlewares/verifyToken');
 router.use(verifyToken);
 
-router.post('/', createSupplierController.handle);
-router.get('/', listSuppliersController.handle);
-router.get('/:id', getSupplierController.handle);
-router.put('/:id', updateSupplierController.handle);
-router.delete('/:id', deleteSupplierController.handle);
+router.post('/', allowSupplierAccess, createSupplierController.handle);
+router.get('/', allowSupplierAccess, listSuppliersController.handle);
+router.get('/:id', allowSupplierAccess, getSupplierController.handle);
+router.put('/:id', allowSupplierAccess, updateSupplierController.handle);
+router.delete('/:id', allowSupplierDelete, deleteSupplierController.handle);
 
 module.exports = router;
