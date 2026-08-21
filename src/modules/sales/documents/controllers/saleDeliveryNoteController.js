c308a047d82222c0fac4aad1e8d197303ca06a78
Arkcom5026 <advicebanphot@gmail.com>
2026-08-21 16:53:16 +0700

    feat(delivery-note): expose revision lineage read controllers

--- src/modules/sales/documents/controllers/saleDeliveryNoteController.js
+++ src/modules/sales/documents/controllers/saleDeliveryNoteController.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const { prisma } = require('../../../../../lib/prisma');
 const { issueSaleDeliveryNote } = require('../issue/issueSaleDeliveryNoteService');
 const { projectCurrentSaleDeliveryNote } = require('../print/projectCurrentSaleDeliveryNoteService');
+const {
+  listDeliveryNoteRevisionHistory,
+  getDeliveryNoteRevisionById,
+} = require('../../delivery-note/lifecycle/deliveryNoteRevisionHistoryService');
 
 const authenticatedBranchId = (req) => {
   const branchId = Number(req.user?.branchId);
@@ -26,6 +31,41 @@ const getSaleDeliveryNote = async (req, res, next) => {
   }
 };
 
+const getSaleDeliveryNoteRevisions = async (req, res, next) => {
+  try {
+    const branchId = authenticatedBranchId(req);
+    if (!branchId) {
+      return res.status(401).json({ code: 'UNAUTHORIZED', message: 'Authenticated branch is required' });
+    }
+    const result = await listDeliveryNoteRevisionHistory({
+      prisma,
+      branchId,
+      saleId: req.params.id,
+    });
+    return res.status(200).json({ ok: true, data: result });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getSaleDeliveryNoteRevision = async (req, res, next) => {
+  try {
+    const branchId = authenticatedBranchId(req);
+    if (!branchId) {
+      return res.status(401).json({ code: 'UNAUTHORIZED', message: 'Authenticated branch is required' });
+    }
+    const result = await getDeliveryNoteRevisionById({
+      prisma,
+      branchId,
+      saleId: req.params.id,
+      revisionId: req.params.revisionId,
+    });
+    return res.status(200).json({ ok: true, data: result });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 const issueSaleDeliveryNoteController = async (req, res, next) => {
   try {
     const branchId = authenticatedBranchId(req);
@@ -44,4 +84,9 @@ const issueSaleDeliveryNoteController = async (req, res, next) => {
   }
 };
 
-module.exports = Object.freeze({ getSaleDeliveryNote, issueSaleDeliveryNoteController });
+module.exports = Object.freeze({
+  getSaleDeliveryNote,
+  getSaleDeliveryNoteRevisions,
+  getSaleDeliveryNoteRevision,
+  issueSaleDeliveryNoteController,
+});
