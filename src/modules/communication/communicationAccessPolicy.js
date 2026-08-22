2dcf4c78e5af5d6b7b9644a202234ad7f8a77068
Arkcom5026 <advicebanphot@gmail.com>
2026-08-22 15:06:05 +0700

    fix(auth): align batch communication capability with Wave 3L

--- src/modules/communication/communicationAccessPolicy.js
+++ src/modules/communication/communicationAccessPolicy.js
@@ -8,7 +8,7 @@ const getCommunicationCapabilities = (actor = {}) => {
   const authenticatedEmployee = Number.isInteger(Number(actor.employeeId)) && Number(actor.employeeId) > 0;
   return Object.freeze({
     viewCommunication:
-      authenticatedEmployee && hasCapability(actor, POSITION_CAPABILITIES.COMMUNICATION_ACCESS),
+      authenticatedEmployee && hasCapability(actor, POSITION_CAPABILITIES.COMMUNICATION_USE),
     manageCommunicationProfiles:
       authenticatedEmployee && hasCapability(actor, POSITION_CAPABILITIES.COMMUNICATION_PROFILE_MANAGE),
   });
